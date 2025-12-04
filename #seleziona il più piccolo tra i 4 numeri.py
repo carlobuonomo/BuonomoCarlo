3303d4edaf57f842ce75931d88ac0b9813cfd4a9
carlobuonomo <carlo.buonomo.s14@liceoviconapoli.it>
2025-12-04 08:36:00 +0100

    Update #seleziona il più piccolo tra i 4 numeri.py

--- #seleziona il più piccolo tra i 4 numeri.py
+++ #seleziona il più piccolo tra i 4 numeri.py
@@ -1,31 +1,35 @@
 #seleziona il più piccolo tra i quattro numeri
 
-a = float(input("inserisci a: "))
-b = float(input("inserisci b: "))
-c = float(input("inserisci c: "))
-d = float(input("inserisci d: "))
-if a>b:
-    if b>c:
-        if c>d:
-         min = d
-        else:
-         min = c
-    else:
-         if b>d:
-          min = d
-         else: 
-          min = b
-else:
-    if a>c:
+def minore(a,b,c,d):
+    if a>b:
+     if b>c:
          if c>d:
           min = d
          else:
           min = c
+     else:
+          if b>d:
+           min = d
+          else: 
+           min = b
     else:
+     if a>c:
+         if c>d:
+          min = d
+         else:
+          min = c
+     else:
          if a>d:
            min = d
          else:
             min = a
+    return min
 
+a = float(input("inserisci a: "))
+b = float(input("inserisci b: "))
+c = float(input("inserisci c: "))
+d = float(input("inserisci d: "))
 
-print ("tra ",a,",",b," , ",c," e ",d," il minore è ",min) 
+m = minore(a,b,c,d)
+
+print ("tra ",a,",",b," , ",c," e ",d," il minore è ",m) 
